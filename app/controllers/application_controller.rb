0bf526cf1236cff16db7a3636b5779644d5934e5
fab835 <fabioczamora@gmail.com>
2013-11-13 12:01:54 -0300

    Layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  
+    protect_from_forgery
+  
+  rescue_from CanCan::AccessDenied do |exception|
+  	flash[:error] = exception.message
+  	render :file => "#{Rails.root}/public/403.html"
+  end
 end
